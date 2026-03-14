c07c60c5936cc884119cb0e24b7f1e8001634671
Sajal Saxena <sajalsaxena2399@gmail.com>
2026-03-14 15:32:32 +0530

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import CartDrawer from "./components/CartDrawer";
 import ValentineBackground from "./components/ValentineBackground";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
